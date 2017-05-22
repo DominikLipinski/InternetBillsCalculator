1d386625a588a5742889f72e817e7d2a7264a229
Dominik Lipinski <maboonga@gmail.com>
2017-05-22 19:42:02 +0200

    started part 2, savings for package B to A displayed

--- src/InternetBills.java
+++ src/InternetBills.java
@@ -18,8 +18,14 @@ public class InternetBills {
         String userInputPackage;
         int userInputHours;
         int exceededHours =0;
+        int exceededHoursB =0;
         double extraCharge =0;
+        double extraChargeB =0;
         double totalBill = 0;
+        double totalBillB = 0;
+        double savingsIfChoseB = 0;
+        double savingsIfChoseC = 0;
+
         System.out.println("What package do you use?");
         userInputPackage = sc.nextLine();
         System.out.println("How many hours did you use?");
@@ -31,19 +37,35 @@ public class InternetBills {
                 extraCharge = exceededHours*PACKAGE_A_EXTRA_FEE;
             }
             totalBill = PACKAGE_A_BASE_PRICE + extraCharge;
+
+            if (userInputHours > PACKAGE_B_HOURS) {
+                exceededHoursB = userInputHours - PACKAGE_B_HOURS;
+                extraChargeB = exceededHoursB * PACKAGE_B_EXTRA_FEE;
+            }
+            totalBillB = PACKAGE_B_BASE_PRICE + extraChargeB;
+
+            if (totalBill>totalBillB){
+                savingsIfChoseB = totalBill - totalBillB;
+                System.out.printf("You would save $%.2f if you've chosen package B\n", savingsIfChoseB);
+            }
+
+
+            //savingsIfChoseB = totalBillC - totalBill;
+
         } else if (userInputPackage.equalsIgnoreCase("b")) {
             if (userInputHours > PACKAGE_B_HOURS) {
                 exceededHours = userInputHours - PACKAGE_B_HOURS;
                 extraCharge = exceededHours * PACKAGE_B_EXTRA_FEE;
             }
             totalBill = PACKAGE_B_BASE_PRICE + extraCharge;
+
+
         } else if (userInputPackage.equalsIgnoreCase("c")) {
             totalBill = PACKAGE_C_BASE_PRICE;
         }
         System.out.printf("Your choosen package is: %s\n Hours exceeded: %d\n " +
                         "Extra fee: $%.2f\n Total bill: $%.2f",
                 userInputPackage, exceededHours, extraCharge, totalBill );
-
     }
 }
 
@@ -62,4 +84,13 @@ public class InternetBills {
 
         Gaddis, Tony (2015-05-29). Starting Out with Java: From Control Structures through Objects (Page 188). Pearson Education.
         Kindle Edition.
+
+        Part 2
+
+    Modify the program so it also calculates and displays the amount of money Package A customers would save
+    if they purchased Package B or C, and the amount of money Package B customers would save if they purchased
+    Package C. If there would be no savings, no message should be printed.
+
+    Gaddis, Tony (2015-05-29). Starting Out with Java: From Control Structures through Objects (Page 188). Pearson Education.
+    Kindle Edition.
 */
